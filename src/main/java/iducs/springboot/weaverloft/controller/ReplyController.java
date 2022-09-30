9262d6d36ab9e2918a336e15ac5b34690da72a7e
GeunHyeong <pg9912@naver.com>
2022-09-30 14:53:26 +0900

    댓글 등록 유효성 검사

--- src/main/java/iducs/springboot/weaverloft/controller/ReplyController.java
+++ src/main/java/iducs/springboot/weaverloft/controller/ReplyController.java
@@ -10,8 +10,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -33,7 +35,7 @@ public class ReplyController {
 
     @PostMapping("")
     // @RequestBody  : JSON으로 들어오는 데이터를 자동으로 해당 타입의 객체로 매핑해주는 역할 -> 별도의 처리 없이도 JSON 데이터를 특정 타입의 객체로 변환해서 처리 가능
-    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO){
+    public ResponseEntity<Long> register(@RequestBody @Valid ReplyDTO replyDTO, BindingResult bindingResult){
 
         Long rno = replyService.register(replyDTO);
 
