f9a53b768f7b3b92475633c4f1963af2f34cebde
GeunHyeong <pg9912@naver.com>
2022-10-12 16:54:37 +0900

    유효성 검사 변경

--- src/main/java/iducs/springboot/weaverloft/domain/MemberDTO.java
+++ src/main/java/iducs/springboot/weaverloft/domain/MemberDTO.java
@@ -8,28 +8,25 @@ import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
 
 @Data //@Getter, @Setter, @EqualsAndHash, @RequiredArgsConstructor
 @Builder
 public class MemberDTO { // DTO(Data Transform Object) : Client <-> Controller <-> Service
 
-    @NotBlank(message = "아이디는 필수 입력 값입니다.")
     @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "아이디는 영어 소문자와 숫자만 사용하여 4~20자리여야 합니다.")
     private String id;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String pw;
 
-    @NotBlank(message = "이름을 입력해주세요.")
     @Pattern(regexp = "^[가-힣]{2,10}$" , message = "이름은 한글 2~10자리여야 합니다.")
     private String name;
 
-    @Email(message = "올바른 이메일 주소를 입력해주세요.")
-    @NotBlank(message = "이메일은 입력해주세요.")
+    @Pattern(regexp = "^[a-z0-9A-Z._-]*@[a-z0-9A-Z]*.[a-zA-Z.]*$",message = "올바른 이메일 주소를 입력해주세요.")
     private String email;
 
-    @NotBlank(message = "휴대폰 번호를 입력해주세요.")
-    @Pattern(regexp = "(01[016789])(\\d{3,4})(\\d{4})", message = "올바른 휴대폰 번호를 입력해주세요.")
+    @Pattern(regexp = "(01[016789])(\\d{3,4})(\\d{4})", message = "-를 제외한 올바른 휴대폰 번호를 입력해주세요.")
     private String phone;
 
     private String address;
@@ -42,6 +39,8 @@ public class MemberDTO { // DTO(Data Transform Object) : Client <-> Controller <
 
     @Builder.Default
     private String role = "user";
+
+    private LocalDateTime pwUpdateDate;
 
 }
 
